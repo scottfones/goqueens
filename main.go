85040e5ae0dcdf0fd5fe888e2553b78506b85025
Scott Fones <scott.fones@gmail.com>
2020-11-21 15:37:51 -0500

    8-quenns form submit working

--- main.go
+++ main.go
@@ -1,38 +1,46 @@
 package main
 
 import (
-	"html/template"
+	"fmt"
 	"log"
 	"net/http"
-	"path/filepath"
+
+	"./web"
 )
 
-var layoutDir string = "web/templates"
-var webpage *template.Template
+// Define Webpages
+var index *web.View
+
+var eightsetup *web.View
+var eightdisplay *web.View
 
 func main() {
-	var err error
-	webpage, err = template.ParseFiles(layoutFiles()...)
-	if err != nil {
-		log.Fatal(err)
-	}
+	index = web.NewView("bootstrap", "web/index.gohtml")
+	eightsetup = web.NewView("bootstrap", "web/eight_setup.gohtml")
+	eightdisplay = web.NewView("bootstrap", "web/eight_display.gohtml")
 
 	http.Handle("/web/", http.StripPrefix("/web/", http.FileServer(http.Dir("web"))))
-	http.HandleFunc("/", handler)
+	http.HandleFunc("/", indexHandler)
+	http.HandleFunc("/eight_setup", eightSetupHandler)
+	http.HandleFunc("/eight_display", eightDisplayHandler)
 
 	log.Println("Listening on :3000...")
 	http.ListenAndServe(":3000", nil)
 
 }
 
-func handler(w http.ResponseWriter, r *http.Request) {
-	webpage.ExecuteTemplate(w, "webpage", nil)
+func indexHandler(w http.ResponseWriter, r *http.Request) {
+	index.Render(w, nil)
 }
 
-func layoutFiles() []string {
-	files, err := filepath.Glob(layoutDir + "/*.gohtml")
-	if err != nil {
-		log.Println(err)
-	}
-	return files
+func eightSetupHandler(w http.ResponseWriter, r *http.Request) {
+	log.Println("Loading: 8-Queens Setup")
+	eightsetup.Render(w, nil)
+}
+
+func eightDisplayHandler(w http.ResponseWriter, r *http.Request) {
+	r.ParseForm()
+    fmt.Println(r.Form)
+	log.Println("Loading: 8-Queens Display")
+	eightdisplay.Render(w, nil)
 }
