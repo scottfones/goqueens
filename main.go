5f36724b91d391ce7301555772cae422bc602ee9
Scott Fones <scott.fones@gmail.com>
2020-11-21 18:36:07 -0500

    added 12 and 16 queens

--- main.go
+++ main.go
@@ -11,18 +11,23 @@ import (
 // Define Webpages
 var index *web.View
 
-var eightsetup *web.View
-var eightdisplay *web.View
+var eightSetup *web.View
+var eightDisplay *web.View
 
-var twelvesetup *web.View
-var twelvedisplay *web.View
+var twelveSetup *web.View
+var twelveDisplay *web.View
+
+var sixteenSetup *web.View
+var sixteenDisplay *web.View
 
 func main() {
 	index = web.NewView("bootstrap", "web/index.gohtml")
-	eightsetup = web.NewView("bootstrap", "web/eight_setup.gohtml")
-	eightdisplay = web.NewView("bootstrap", "web/eight_display.gohtml")
-	twelvesetup = web.NewView("bootstrap", "web/twelve_setup.gohtml")
-	twelvedisplay = web.NewView("bootstrap", "web/twelve_display.gohtml")
+	eightSetup = web.NewView("bootstrap", "web/eight_setup.gohtml")
+	eightDisplay = web.NewView("bootstrap", "web/eight_display.gohtml")
+	twelveSetup = web.NewView("bootstrap", "web/twelve_setup.gohtml")
+	twelveDisplay = web.NewView("bootstrap", "web/twelve_display.gohtml")
+	sixteenSetup = web.NewView("bootstrap", "web/sixteen_setup.gohtml")
+	sixteenDisplay = web.NewView("bootstrap", "web/sixteen_display.gohtml")
 
 	http.Handle("/web/", http.StripPrefix("/web/", http.FileServer(http.Dir("web"))))
 	http.HandleFunc("/", indexHandler)
@@ -30,6 +35,8 @@ func main() {
 	http.HandleFunc("/eight_display", eightDisplayHandler)
 	http.HandleFunc("/twelve_setup", twelveSetupHandler)
 	http.HandleFunc("/twelve_display", twelveDisplayHandler)
+	http.HandleFunc("/sixteen_setup", sixteenSetupHandler)
+	http.HandleFunc("/sixteen_display", sixteenDisplayHandler)
 
 	log.Println("Listening on :3000...")
 	http.ListenAndServe(":3000", nil)
@@ -42,24 +49,36 @@ func indexHandler(w http.ResponseWriter, r *http.Request) {
 
 func eightSetupHandler(w http.ResponseWriter, r *http.Request) {
 	log.Println("Loading: 8-Queens Setup")
-	eightsetup.Render(w, nil)
+	eightSetup.Render(w, nil)
 }
 
 func eightDisplayHandler(w http.ResponseWriter, r *http.Request) {
 	r.ParseForm()
 	fmt.Println(r.Form)
 	log.Println("Loading: 8-Queens Display")
-	eightdisplay.Render(w, nil)
+	eightDisplay.Render(w, nil)
 }
 
 func twelveSetupHandler(w http.ResponseWriter, r *http.Request) {
 	log.Println("Loading: 12-Queens Setup")
-	twelvesetup.Render(w, nil)
+	twelveSetup.Render(w, nil)
 }
 
 func twelveDisplayHandler(w http.ResponseWriter, r *http.Request) {
 	r.ParseForm()
 	fmt.Println(r.Form)
 	log.Println("Loading: 12-Queens Display")
-	twelvedisplay.Render(w, nil)
+	twelveDisplay.Render(w, nil)
+}
+
+func sixteenSetupHandler(w http.ResponseWriter, r *http.Request) {
+	log.Println("Loading: 16-Queens Setup")
+	sixteenSetup.Render(w, nil)
+}
+
+func sixteenDisplayHandler(w http.ResponseWriter, r *http.Request) {
+	r.ParseForm()
+	fmt.Println(r.Form)
+	log.Println("Loading: 16-Queens Display")
+	sixteenDisplay.Render(w, nil)
 }
