cf2462e8d831de7989a45e9bacd3ae7e984f2258
Scott Fones <scott.fones@gmail.com>
2020-11-21 18:08:47 -0500

    init 12 setup

--- main.go
+++ main.go
@@ -14,15 +14,22 @@ var index *web.View
 var eightsetup *web.View
 var eightdisplay *web.View
 
+var twelvesetup *web.View
+var twelvedisplay *web.View
+
 func main() {
 	index = web.NewView("bootstrap", "web/index.gohtml")
 	eightsetup = web.NewView("bootstrap", "web/eight_setup.gohtml")
 	eightdisplay = web.NewView("bootstrap", "web/eight_display.gohtml")
+	twelvesetup = web.NewView("bootstrap", "web/twelve_setup.gohtml")
+	twelvedisplay = web.NewView("bootstrap", "web/twelve_display.gohtml")
 
 	http.Handle("/web/", http.StripPrefix("/web/", http.FileServer(http.Dir("web"))))
 	http.HandleFunc("/", indexHandler)
 	http.HandleFunc("/eight_setup", eightSetupHandler)
 	http.HandleFunc("/eight_display", eightDisplayHandler)
+	http.HandleFunc("/twelve_setup", twelveSetupHandler)
+	http.HandleFunc("/twelve_display", twelveDisplayHandler)
 
 	log.Println("Listening on :3000...")
 	http.ListenAndServe(":3000", nil)
@@ -40,7 +47,19 @@ func eightSetupHandler(w http.ResponseWriter, r *http.Request) {
 
 func eightDisplayHandler(w http.ResponseWriter, r *http.Request) {
 	r.ParseForm()
-    fmt.Println(r.Form)
+	fmt.Println(r.Form)
 	log.Println("Loading: 8-Queens Display")
 	eightdisplay.Render(w, nil)
+}
+
+func twelveSetupHandler(w http.ResponseWriter, r *http.Request) {
+	log.Println("Loading: 12-Queens Setup")
+	twelvesetup.Render(w, nil)
+}
+
+func twelveDisplayHandler(w http.ResponseWriter, r *http.Request) {
+	r.ParseForm()
+	fmt.Println(r.Form)
+	log.Println("Loading: 12-Queens Display")
+	twelvedisplay.Render(w, nil)
 }
