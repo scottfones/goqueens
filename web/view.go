fb9aa782de90a1345a3699a02851729393632fd9
Scott Fones <scott.fones@gmail.com>
2020-11-21 15:40:22 -0500

    checkpoint 8q form submit

---
+++ web/view.go
@@ -0,0 +1,44 @@
+package web
+
+import (
+	"html/template"
+	"log"
+	"net/http"
+	"path/filepath"
+)
+
+var layoutDir string = "web/templates"
+
+// NewView prepares a view for export.
+func NewView(layout string, files ...string) *View {
+	files = append(layoutFiles(), files...)
+	t, err := template.ParseFiles(files...)
+	if err != nil {
+		log.Println(err)
+	}
+
+	return &View{
+		Template: t,
+		Layout:   layout,
+	}
+}
+
+// View consists of a template and layout.
+type View struct {
+	Template *template.Template
+	Layout   string
+}
+
+// Render returns a constructed template
+func (v *View) Render(w http.ResponseWriter, data interface{}) error {
+	return v.Template.ExecuteTemplate(w, v.Layout, data)
+}
+
+// layoutFiles adds all gohtml files to a slice
+func layoutFiles() []string {
+	files, err := filepath.Glob(layoutDir + "/*.gohtml")
+	if err != nil {
+		log.Println(err)
+	}
+	return files
+}
