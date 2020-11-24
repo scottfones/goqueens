add7a145bfff5dc91e7e032e97c83dd5d4761456
Scott Fones <scott.fones@gmail.com>
2020-11-24 09:08:47 -0500

    working to figure 1

--- game/csv.go
+++ game/csv.go
@@ -10,8 +10,8 @@ type assignment struct {
 }
 
 type constraint struct {
-	q1       *queen
-	q2       *queen
+	q1       int
+	q2       int
 	assSlice []assignment
 }
 
@@ -40,13 +40,13 @@ func (c *csp) createConstraints() {
 
 	for i, p1 := range c.queens[0 : n-1] {
 		for _, p2 := range c.queens[i+1 : n] {
-			con := constraint{q1: &p1, q2: &p2}
-			log.Println("Q1: ", p1)
-			log.Println("Q2: ", p2)
+			con := constraint{q1: p1.col, q2: p2.col}
+			//log.Println("Q1: ", p1)
+			//log.Println("Q2: ", p2)
 
 			for _, r1 := range p1.domain {
 				for _, r2 := range p2.domain {
-					log.Println("Comparing: (", p1.col, ",", r1, ") and (", p2.col, ",", r2, ")")
+					//log.Println("Comparing: (", p1.col, ",", r1, ") and (", p2.col, ",", r2, ")")
 					if !(p1.isConflict(&p2, r1, r2)) {
 						//log.Println("No Conflict")
 						a := assignment{r1, r2}
@@ -54,7 +54,7 @@ func (c *csp) createConstraints() {
 					}
 				}
 			}
-			log.Println("Assigning: ", con.assSlice)
+			//log.Println("Assigning: ", con.assSlice)
 			c.conSlice = append(c.conSlice, con)
 		}
 	}
@@ -81,7 +81,8 @@ func (c *csp) print(i int) {
 	log.Println()
 	log.Println("Constraints:")
 	for _, con := range c.conSlice {
-		log.Println("(", con.q1, ", ", con.q2, "): ", con.assSlice)
+		//log.Println("(", con, ", ", con[1], "): ", con.assSlice)
+		log.Println(con)
 	}
 }
 
@@ -91,7 +92,7 @@ func NewGame(initQs []int) {
 	var game = csp{size: len(initQs)}
 	game.createQueens(initQs)
 	game.createConstraints()
-	//game.print(0)
+	game.print(0)
 	//jsonStr, _ := json.Marshal(game)
 	//log.Println(string(jsonStr))
 	//log.Println(game)
