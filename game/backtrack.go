8cfe6c24cce2c468d8863f571f6461faac209693
Scott Fones <scott.fones@gmail.com>
2020-12-01 17:48:51 -0500

    added comments. TODO: docstrings

--- game/backtrack.go
+++ game/backtrack.go
@@ -14,12 +14,15 @@ func mrv(c *csp) Queen {
 	for i := range c.queens {
 		q := c.queens[i]
 
+		// If row has been assigned
 		if q.Row != -1 {
 			continue
 
+		// If minq hasn't been assigned
 		} else if minq.Row == 0 {
 			minq = q
 
+		// Update to more constrained domain
 		} else if len(q.Domain) < len(minq.Domain) {
 			minq = q
 		}
@@ -39,20 +42,25 @@ func backtrack(c *csp, assignments []Solution) []Solution {
 	for _, val := range pivotQueen.Domain {
 		log.Println("Domain Value:", val)
 
+		// Move forward with copy to ease backtracking
 		tcsp := c.getCopy()
 		tq := &tcsp.queens[pivotQueen.Col-1]
 		tq.Row = val
 		tq.Domain = []int{val}
 
 		if ac3(&tcsp) {
+			// Construct solution and append
 			s := Solution{pivotQueen.Col, val, tcsp.queens}
 			assignments = append(assignments, s)
+
 			tresult := backtrack(&tcsp, assignments)
 
+			// If tresult is not the fail state
 			if len(tresult) > 0 {
 				log.Println("Solution Length:", len(tresult))
 				return tresult
 			}
+			// If fail state, remove from assignments
 			assignments = assignments[:len(assignments)-1]
 		}
 	}
